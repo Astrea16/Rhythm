82899ed8dc3a29e5ce5b243ce81238d7628d4787
Astrea16 <7yatan@gmail.com>
2022-05-26 15:18:44 +0300

    Initial project version

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+const swiper = new Swiper('.swiper-reviews', {
+	autoHeight: true,
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+	el: '.swiper-reviews__pagination',
+	clickable: true,
+	},
+
+});
+}
